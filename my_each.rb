a318714d488825c30b9beb034b6a416ec7dae35f
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-16 10:41:15 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
 def my_each # put argument(s) here
   # code here
 end
+
+
+
+
+def hello_t(array)
+
+    i=0 
+    while i<array.length 
+      puts "Hello #{array[i]}."
+      i+=1
+    end
+    array
+
+end
